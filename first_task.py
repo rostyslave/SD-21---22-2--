0b44216198da9b2f4f17b881d30878c539dc47e2
rostyslave <rostyslav.r.burtnyk@ukd.edu.ua>
2025-02-19 11:15:49 +0200

    git add Task completed

--- first_task.py
+++ first_task.py
@@ -1,2 +1,4 @@
 print("Hi, my name is Rostyslav and I’m studying software development in college")
 print("Hi, my name is Andriy and I’m studying software development in college")
+
+#Task completed
