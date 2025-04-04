58033d008c0785363bc0bf7fff893d8295048ff9
rostyslave <rostyslav.r.burtnyk@ukd.edu.ua>
2025-04-04 16:02:09 +0300

    Я встановив пакет файлів Django та створив проект для практичної роботи і в проект додав superuser(суперкористувача), після того як прописав "python manage.py startapp polls" додав додаток до встановлених додатків у файл settings.py Після цього запустив сервер за допомогою команди "python manage.py runserver" Згодом у файлі models.py я створив два класи, class Question та class Choice та згодом виконав міграцію змін в БД.

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from polls.models import Question, Choice
+
+@admin.register(Question)
+class QuestionModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class ChoiceModelAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
