57cb6ebb55e14657b3a8eecf4d1094bde8365beb
rostyslave <rostyslav.r.burtnyk@ukd.edu.ua>
2025-04-15 11:06:09 +0300

    Я розробив та впровадив системи реєстрації користувачів у моєму веб-додатку Завдання полягало у тому, щоб я створив Django View та Form для реалізації функціоналу користувача Створив форми реєстрації у аплікації polls та створив файл forms.py далі реалізував Views для реєстрації на моєму сайті Згодом створив шаблон реєстрації для повного функціонування сторінки та налаштував url маршрути які після реєстрації перекидують на головну сторінку там де знаходяться завдання

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,9 @@
+from polls import views
 from polls.views import question_list
 from django.urls import path
 
 app_name = 'polls'
 urlpatterns = [
     path('', question_list, name='question_list'),
+    path('register/', views.register_view, name='register'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from .Forms import UserRegistrationForm
 from .models import Question
 
 def question_list(request):
@@ -9,3 +11,19 @@ def question_list(request):
     context = {'questions': questions}
     return render(request, 'question_list.html', context)
 
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('polls:question_list')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {"form": form})
